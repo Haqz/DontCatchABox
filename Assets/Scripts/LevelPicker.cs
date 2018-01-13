6f5dbe27a334eafe21e12358b9c395bb3b394e55
Haqz <maszynista91@gmail.com>
2018-01-13 22:42:03 +0100

    0.2.2

--- Assets/Scripts/LevelPicker.cs
+++ Assets/Scripts/LevelPicker.cs
@@ -3,56 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
-public class LevelPicker: MonoBehaviour {
-    public GameObject[] level;
-    public int choosed = 1;
-    public Button NextButton;
-    public Button BackButton;
-    public Animator anim;
-	private UnityAction<int> m_MyFirstAction;
+using UnityEngine.SceneManagement;
 
-	// Use this for initialization
-	void Start () {
-        level = GameObject.FindGameObjectsWithTag("LevelPick");
-		m_MyFirstAction += TaskOnClick;
-		Button btn = NextButton.GetComponent<Button>();
-        Button btn1 = BackButton.GetComponent<Button>();
-		btn.onClick.AddListener(() => TaskOnClick(1));
-		btn1.onClick.AddListener(() => TaskOnClick(2));
-        choosed = 1;
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        anim.SetInteger("Choosed", choosed);
-        PlayerPrefs.SetInt("choosed", choosed);
+public class LevelPicker: MonoBehaviour {
+
+	private const string LevelScenePrefix = "level";
+	private const string LevelPickerScene = "levelPicker";
+	public int SelectedLevel;
+
+	public void LoadLevelPicker()
+	{
+		SceneManager.LoadScene(LevelPickerScene);
 	}
-    void TaskOnClick(int ch)
-    {
-		switch (ch)
-		{
-			case 1:
-				if (choosed != level.Length - 1)
-				{
-					choosed++;
-				}
-				else
-				{
-					choosed = level.Length - 1;
-				}
-				break;
-			case 2:
-				if (choosed != 0)
-				{
-					choosed--;
-				}
-				else
-				{
-					choosed = 0;
-				}
-				break;
-		}
-		
-    }
+	public void LoadSelectedLevel()
+	{
+		LoadLevel(SelectedLevel);
+	}
+	public void LoadLevel(int levelName)
+	{
+
+		Debug.Log(levelName);
+	}
+	public void NextLevel()
+	{
+		SelectedLevel++;
+	}
+	public void PreviousLevel()
+	{
+		SelectedLevel--;
+	}
 }
